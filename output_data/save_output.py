caabc8cb2714226cbb96c452176d5bae8e94be97
yangbranden <byang64@asu.edu>
2024-09-30 13:38:19 -0700

    script to view result of test (using session id)

---
+++ output_data/save_output.py
@@ -0,0 +1,55 @@
+import requests
+import os
+import json
+import yaml
+
+session_id = "b4e4f28a449224a6586f55930e2ba56ebf03f28d"
+
+urls_visited = []
+phishing_urls = []
+
+with open("./urls/latest.yml") as f:
+    y = yaml.safe_load(f)
+    phishing_urls = y["urls"]
+
+print(phishing_urls)
+
+s = requests.Session()
+s.auth = (os.environ.get("BROWSERSTACK_USERNAME"), os.environ.get("BROWSERSTACK_ACCESS_KEY"))
+
+r = s.get(f"https://api.browserstack.com/automate/sessions/{session_id}/logs")
+lines = r.text.splitlines()
+# print(lines)
+
+with open("./output_data/tmp.json", "w") as f:
+    for line in lines:
+        f.write(r.text)
+
+output = dict()
+current_entry = dict()
+for line in lines:
+    if "REQUEST" not in line:
+        continue
+    segments = line.split(' ')
+    json_str = ''.join(segments[7:])
+    
+    # Attempt to parse as JSON
+    try:
+        json_data = json.loads(json_str)
+        # print(json_data)
+        if "/url" in line:
+            current_entry["url"] = json_data["url"]
+        elif "/elements" in line:
+            current_entry["using"] = json_data["using"]
+        elif "/execute/sync" in line:
+            result = json.loads(json_data["script"].split("browserstack_executor:")[-1])
+            current_entry["script"] = result["arguments"]
+            output[current_entry["url"]] = current_entry["script"]
+    except json.JSONDecodeError:
+        print(f"Last segment is not valid JSON: {json_str}")
+
+with open("./output_data/output.json", "w") as f:
+    json.dump(output, f, indent=4)
+
+print(output)
+print("Check output.json for cleaner view of output.")
