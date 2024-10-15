910ce5d079001244782d8dcd4d95f05bde3050f9
yangbranden <byang64@asu.edu>
2024-10-15 12:48:22 -0700

    moving target_generator to be under browserstack module (other IaaS providers may not use same target platforms format)

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import sys
 from omegaconf import OmegaConf
 
 from src.fetch_phishtank import PhishtankFetcher
-from src.target_generator import TargetGenerator
 from src.browserstack.browserstack_runner import BrowserstackRunner
 
 CONFIG_FILE = "config.yml"
@@ -15,7 +14,7 @@ def browserstack_runner(args):
 
 def target_generator(args):
     config = OmegaConf.load(CONFIG_FILE)
-    x = TargetGenerator(config=config)
+    x = BrowserstackRunner(config=config)
     x.generate_targets(args.platform)
 
 def phishtank_fetcher(args):
@@ -25,18 +24,20 @@ def phishtank_fetcher(args):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Command-line tool to run different modules.")
+    parser = argparse.ArgumentParser(description="Mobile Phishing framework")
     subparsers = parser.add_subparsers(dest="module", required=True, help="Module to run")
 
-    # Subparser for browserstack_runner
-    browserstack_runner_parser = subparsers.add_parser("browserstack", help="Use browserstack_runner submodule")
-    # options for this module should be specified in config.yml
-    browserstack_runner_parser.set_defaults(func=browserstack_runner)
-    
-    # Subparser for target_generator
-    target_generator_parser = subparsers.add_parser("target_generator", help="Use target_generator submodule")
-    target_generator_parser.add_argument("-p", "--platform", choices=['all', 'android', 'ios', 'windows', 'macosx'], default='all', required=True, help="Value must be among [all, android, ios, windows, macosx]")
-    target_generator_parser.set_defaults(func=target_generator)
+    # Subparser for browserstack
+    browserstack_parser = subparsers.add_parser("browserstack", help="Use browserstack submodule")
+    browserstack_subparsers = browserstack_parser.add_subparsers(dest="browserstack_module", required=True, help="Browserstack submodule to run")
+    # Subsubparser for generating browserstack targets
+    browserstack_generate_targets = browserstack_subparsers.add_parser("generate_targets", help="Generate target list to be used by browserstack")
+    browserstack_generate_targets.add_argument("-p", "--platform", choices=['all', 'android', 'ios', 'windows', 'macosx'], default='all', required=True, help="Value must be among [all, android, ios, windows, macosx]")
+    browserstack_generate_targets.set_defaults(func=target_generator)
+    # Subsubparser for running browserstack
+    browserstack_exec = browserstack_subparsers.add_parser("exec", help="Execute browserstack using the configuration in the config.yml file")
+    # other options for this module should be specified in config.yml
+    browserstack_exec.set_defaults(func=browserstack_runner)
 
     # Subparser for phishtank_fetcher
     phishtank_fetcher_parser = subparsers.add_parser("phishtank_fetcher", help="Use phishtank_fetcher submodule")
--- src/browserstack/browserstack_runner.py
+++ src/browserstack/browserstack_runner.py
@@ -10,6 +10,7 @@ from dataclasses import dataclass
 from omegaconf import DictConfig, ListConfig
 
 from src.platforms import Platforms
+from src.util import write_file_source_header
 
 @dataclass
 class BrowserstackRunner:
@@ -74,3 +75,215 @@ class BrowserstackRunner:
         # Reset back to the original config
         with open("browserstack.yml", "w") as f:
             yaml.dump(original_config, f)
+    
+    def generate_targets(self, output_mode):
+        if output_mode == "all":
+            output_mode = Platforms.ALL
+        if output_mode == "android":
+            output_mode = Platforms.ANDROID
+        if output_mode == "ios":
+            output_mode = Platforms.IOS
+        if output_mode == "windows":
+            output_mode = Platforms.WINDOWS
+        if output_mode == "macosx":
+            output_mode = Platforms.MACOSX
+
+        assert output_mode in Platforms, "Error: OUTPUT_MODE must be among [ALL, ANDROID, IOS, WINDOWS, MACOSX]; NOTE: this function should not be being called directly.\n"
+
+        # Setup output location
+        base_dir = self.config.browserstack_runner.target_generator.targets_directory
+        if output_mode == Platforms.ALL:
+            output_location = f"{base_dir}/all_targets"
+        elif output_mode == Platforms.ANDROID:
+            output_location = f"{base_dir}/android"
+        elif output_mode == Platforms.IOS:
+            output_location = f"{base_dir}/ios"
+        elif output_mode == Platforms.WINDOWS:
+            output_location = f"{base_dir}/windows"
+        elif output_mode == Platforms.MACOSX:
+            output_location = f"{base_dir}/macosx"
+        if self.config.browserstack_runner.target_generator.custom_outfile is not None:
+            output_location = self.config.browserstack_runner.target_generator.custom_outfile
+
+        # Setup for if we are scoping browser versions
+        SCOPE_BROWSER_VERSIONS = False
+        if self.config.browserstack_runner.target_generator.browser_versions_file is not None:
+            SCOPE_BROWSER_VERSIONS = True
+
+        if SCOPE_BROWSER_VERSIONS:
+            try:
+                with open(f"{self.config.browserstack_runner.target_generator.browser_versions_file}", "r") as f:
+                    browser_versions = yaml.load(f)
+                    firefox_versions_range = browser_versions["firefox_versions"]
+                    chrome_versions_range = browser_versions["chrome_versions"]
+                    edge_versions_range = browser_versions["edge_versions"]
+                    safari_versions_range = browser_versions["safari_versions"]
+                    opera_versions_range = browser_versions["opera_versions"]
+            except Exception as e:
+                print(f"Unable to open {self.config.browserstack_runner.target_generator.browser_versions_file}; file may not exist. Defaulting to all browser versions. (Error: {e})")
+
+        # Use API to get all possible combinations of browser versions
+        s = requests.Session()
+        s.auth = (os.environ.get("BROWSERSTACK_USERNAME"), os.environ.get("BROWSERSTACK_ACCESS_KEY"))
+
+        # Make request to API to gather all possible platform combinations
+        r = s.get("https://api.browserstack.com/automate/browsers.json")
+        output = json.loads(r.text)
+
+        # Remove null values to avoid issues when running browserstack-sdk
+        for item in output:
+            null_fields = [field for field in item if item[field] is None]
+            for n in null_fields:
+                item.pop(n)
+
+        # Filter by OS depending on output mode
+        selective_output = []
+        if output_mode == Platforms.ANDROID:
+            for item in output:
+                if item["os"] == "android":
+                    selective_output.append(item)
+            output = selective_output
+        elif output_mode == Platforms.IOS:
+            for item in output:
+                if item["os"] == "ios":
+                    selective_output.append(item)
+            output = selective_output
+        elif output_mode == Platforms.WINDOWS:
+            for item in output:
+                if item["os"] == "Windows":
+                    selective_output.append(item)
+            output = selective_output
+        elif output_mode == Platforms.MACOSX:
+            for item in output:
+                if item["os"] == "OS X":
+                    selective_output.append(item)
+            output = selective_output
+        
+        # Filter by browser versions (if specified)
+        if SCOPE_BROWSER_VERSIONS:
+            selective_output = []
+            for item in output:
+                # just add the mobile devices; we are unable to specify browser versions
+                if item["os"] == "android" or item["os"] == "ios":
+                    selective_output.append(item)
+                    continue
+                try:
+                    detected_version = float(item["browser_version"])
+                    if item["browser"] == "firefox":
+                        if detected_version in firefox_versions_range:
+                            selective_output.append(item)
+                    elif item["browser"] == "chrome":
+                        if detected_version in chrome_versions_range:
+                            selective_output.append(item)
+                    elif item["browser"] == "edge":
+                        if detected_version in edge_versions_range:
+                            selective_output.append(item)
+                    elif item["browser"] == "safari":
+                        if detected_version in safari_versions_range:
+                            selective_output.append(item)
+                    elif item["browser"] == "opera":
+                        if detected_version in opera_versions_range:
+                            selective_output.append(item)
+                except Exception as e:
+                    # print(e)
+                    continue
+            output = selective_output
+
+            # Detect if we are outputting as a single file or directory
+            SINGLE_FILE = self.config.browserstack_runner.target_generator.output_as_file
+
+            # Create the output directory; if it already exists, remove existing output
+            targets_dir = os.path.dirname(output_location)
+            if not os.path.exists(targets_dir):
+                os.makedirs(targets_dir)
+            if SINGLE_FILE:
+                if os.path.exists(output_location):
+                    for root, dirs, files in os.walk(output_location, topdown=False):
+                        for file in files:
+                            os.remove(os.path.join(root, file))
+                        for dir in dirs:
+                            os.rmdir(os.path.join(root, dir))
+                    os.rmdir(output_location)
+                os.makedirs(output_location)
+
+            if SINGLE_FILE:
+                with open(f"{output_location}.yml", "w") as f:
+                    write_file_source_header("generate_targets (browserstack_runner.py)", f)
+                    yaml.dump(output, f)
+            else:
+                # Separate output into files each containing only n entries
+                for i in range(0, len(output), self.config.browserstack_runner.target_generator.entries_per_file):
+                    batch = output[i:i+self.config.browserstack_runner.target_generator.entries_per_file]
+                    file_index = i // self.config.browserstack_runner.target_generator.entries_per_file
+                    with open(f"{output_location}/{file_index}.yml", "w") as f:
+                        write_file_source_header("generate_targets (browserstack_runner.py)", f)
+                        yaml.dump(batch, f)
+
+    # TODO: This currently does not perform any sort of useful analysis; 
+    # plan was to use this to programmatically go through the browser changelogs and make a list of versions
+    # that have significant security updates
+    def scope_browser_versions(self):
+        # Use API to get all possible combinations of browser versions
+        s = requests.Session()
+        s.auth = (os.environ.get("BROWSERSTACK_USERNAME"), os.environ.get("BROWSERSTACK_ACCESS_KEY"))
+
+        r = s.get("https://api.browserstack.com/automate/browsers.json")
+        output = json.loads(r.text)
+
+        # Find the latest version
+        latest_versions = {
+            'firefox': 0.0,
+            'chrome': 0.0,
+            'edge': 0.0,
+            'safari': 0.0,
+            'opera': 0.0
+        }
+        for item in output:
+            if item["browser"] == "firefox":
+                try:
+                    detected_version = float(item["browser_version"])
+                    if detected_version > latest_versions["firefox"]:
+                        latest_versions["firefox"] = detected_version
+                except Exception:
+                    continue
+            elif item["browser"] == "chrome":
+                try:
+                    detected_version = float(item["browser_version"])
+                    if detected_version > latest_versions["chrome"]:
+                        latest_versions["chrome"] = detected_version
+                except Exception:
+                    continue
+            elif item["browser"] == "edge":
+                try:
+                    detected_version = float(item["browser_version"])
+                    if detected_version > latest_versions["edge"]:
+                        latest_versions["edge"] = detected_version
+                except Exception:
+                    continue
+            elif item["browser"] == "safari":
+                try:
+                    detected_version = float(item["browser_version"])
+                    if detected_version > latest_versions["safari"]:
+                        latest_versions["safari"] = detected_version
+                except Exception:
+                    continue
+            elif item["browser"] == "opera":
+                try:
+                    detected_version = float(item["browser_version"])
+                    if detected_version > latest_versions["opera"]:
+                        latest_versions["opera"] = detected_version
+                except Exception:
+                    continue
+        print("Latest versions:\n", latest_versions)
+
+        data = {
+            'firefox_versions': [latest_versions["firefox"] - i for i in range(10)],
+            'chrome_versions': [latest_versions["chrome"] - i for i in range(10)],
+            'edge_versions': [latest_versions["edge"] - i for i in range(10)],
+            'safari_versions': [latest_versions["safari"] - i for i in range(10)],
+            'opera_versions': [12.16, 12.15]
+        }
+
+        with open("./targets/browser_versions.yml", "w+") as f:
+            write_file_source_header("scope_browser_versions (browserstack_runner.py)", f)
+            yaml.dump(data, f)
