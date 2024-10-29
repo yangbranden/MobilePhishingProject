fcf6e6ebf325d2d6f2a966ea16091b55d4727df6
yangbranden <byang64@asu.edu>
2024-10-29 14:49:53 -0700

    scoping browser versions

--- src/browserstack/browserstack_runner.py
+++ src/browserstack/browserstack_runner.py
@@ -5,6 +5,11 @@ import requests
 import json
 from datetime import datetime
 from user_agents import parse
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 yaml = ruamel.yaml.YAML() # using this version of yaml to preserve comments
 
 from dataclasses import dataclass
@@ -305,18 +310,129 @@ class BrowserstackRunner:
                     continue
         print("Latest versions:\n", latest_versions)
 
+        # CVEDetails search URLs
+        cvedetails_urls = {
+            "firefox": "https://www.cvedetails.com/vulnerability-list/vendor_id-452/product_id-3264/Mozilla-Firefox.html",
+            "chrome": "https://www.cvedetails.com/vulnerability-list/vendor_id-1224/product_id-15031/Google-Chrome.html",
+            "edge": "https://www.cvedetails.com/vulnerability-list/vendor_id-26/product_id-32367/Microsoft-Edge.html",
+            "safari": "https://www.cvedetails.com/vulnerability-list/vendor_id-49/product_id-2935/Apple-Safari.html"
+        }
+
+        cve_results = {
+            "firefox": [],
+            "chrome": [],
+            "edge": [],
+            "safari": []
+        }
+
+        # Number of CVEs to record for each browser
+        cve_count = {
+            "firefox": 20,
+            "chrome": 30,
+            "edge": 20,
+            "safari": 20
+        }
+
+        # Keywords to search by on CVEs:
+        phishing_keywords = ["spoof", "spoofing", "fake", "phishing"]
+
+        # Use selenium to search CVEDetails website and record a list of CVEs that have the keywords in them 
+        driver = webdriver.Chrome(options=ChromeOptions())
+        
+        # Search each CVEdetails page
+        for browser, url in cvedetails_urls.items():
+            # Visit the search page
+            driver.get(url)
+            wait = WebDriverWait(driver, 30)
+
+            # Keep going until number of results met for each browser
+            while len(cve_results[browser]) < cve_count[browser]:
+                # Find all CVE elements on the page
+                cve_elements = driver.find_elements(By.XPATH, "//div[@id='searchresults']/div[@data-tsvfield='cveinfo']")
+
+                for cve_element in cve_elements:
+                    # Parse the cve_element
+                    lines = cve_element.text.split("\n")  # Split by new lines
+                    cve_id = lines[0].strip()
+                    summary = lines[1].strip()
+
+                    # print((cve_id, summary))
+
+                    # Check if any keyword is present in the description
+                    if any(keyword.lower() in summary.lower() for keyword in phishing_keywords):
+                        print("entry found:", (cve_id, summary))
+                        cve_results[browser].append((cve_id, summary))
+
+                # Go to the next page
+                next_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@title='Next page']")))
+                next_button.click()
+
+        driver.quit()
+
+        with open("./relevant_cves.yml", "w+") as f:
+            yaml.dump(cve_results, f)
+
+        # THIS IS JUST EXTRA; we can also just manually parse the version if it's too hard to sort through this programmatically
+        # Then sort through each of the CVEs and their summaries; get the versions from them (using sets so no duplicates)
+        versions = {
+            "firefox": set(),
+            "chrome": set(),
+            "edge": set(),
+            "safari": set()
+        }
+
+        # Parse versions for Firefox; format is "Firefox < ###.#"
+        for cve_id, description in cve_results["firefox"]:
+            # Find the starting index of "Firefox < "
+            start_index = description.find("Firefox < ")
+            # If the format "Firefox < " is found in the description
+            if start_index != -1:
+                # Extract the version by slicing the string after "Firefox < "
+                version_part = description[start_index + len("Firefox < "):]  
+                # Find the end of the version number, which is usually the first space or period afterward
+                end_index = version_part.find(" ")
+                if end_index == -1:  # If no space, take the whole part
+                    end_index = len(version_part)
+                version_str = version_part[:end_index].rstrip(",.")
+                # Add the version to the Firefox set
+                if version_str.isdigit():
+                    versions["firefox"].add(int(version_str))
+        versions["firefox"] = sorted(versions["firefox"])
+        print("Extracted Firefox Versions:", versions["firefox"])
+
+
+        # Parse versions for Google Chrome; format is "Google Chrome prior to ###.#"
+        for cve_id, description in cve_results["chrome"]:
+            if "Google Chrome prior to" in description:
+                # Extract the version substring following "Google Chrome prior to "
+                start_index = description.index("Google Chrome prior to") + len("Google Chrome prior to ")
+                version_part = description[start_index:].split('.')[0]  # Get the major version before the first period
+                # Convert it to an integer
+                version_number = int(version_part)
+                # Add it to the set of Chrome versions
+                versions["chrome"].add(version_number)
+        # Convert the set to a sorted list
+        versions["chrome"] = sorted(versions["chrome"])
+        print("Extracted Chrome Versions:", versions["chrome"])
+
+
+        # (Versions for Microsoft Edge must be parsed manually; summary texts do not specify, but it does show on CVEdetails website)
+
+        # Parse versions for Safari; format is "Safari ##.#"
+
+
         # TEMPORARY: get latest 10 versions of each (except Opera because there's only 2 available and they're on Windows XP lol)
         data = {
-            'firefox_versions': [latest_versions["firefox"] - i for i in range(10)],
-            'chrome_versions': [latest_versions["chrome"] - i for i in range(10)],
+            'firefox_versions': versions["firefox"],
+            'chrome_versions': versions["chrome"],
             'edge_versions': [latest_versions["edge"] - i for i in range(10)],
             'safari_versions': [latest_versions["safari"] - i for i in range(10)],
             'opera_versions': [12.16, 12.15]
         }
 
-        with open("./targets/browser_versions.yml", "w+") as f:
-            write_file_source_header("scope_browser_versions (browserstack_runner.py)", f)
-            yaml.dump(data, f)
+        # with open("./targets/browser_versions.yml", "w+") as f:
+        #     # write_file_source_header("scope_browser_versions (browserstack_runner.py)", f)
+        #     yaml.dump(data, f)
 
 
     # Gather all relevant session ids based on a unique identifier in the title of the associated build(s)
