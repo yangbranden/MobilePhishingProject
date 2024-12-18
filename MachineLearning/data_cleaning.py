1b556577d0e43809c2c280d1935a3a967c6b8c1c
yangbranden <byang64@asu.edu>
2024-12-18 04:21:14 -0700

    idk what im doing

--- MachineLearning/data_cleaning.py
+++ MachineLearning/data_cleaning.py
@@ -1,10 +1,15 @@
 # Goal of this script is to take the raw data from our tests and convert it into a CSV file with valuable output
 # Input:
 #   - text_logs.txt
-#   - network_logs.txt 
+#   - network_logs.txt
 #   - page_sources.json
 #
-# Output:
-#   - 
+# Output (fields of CSV file):
+#   - url (URL of website visited)
+#   - start_time (timestamp indicating time when first request in visit began)
+#   - phishing (whether the website is phishing or benign)
+#   - All request headers (bitmap of all values in the network requests associated with a website visit)
+#   - All response headers (bitmap of all values in the network responses associated with a website visit)
+
 
 # TODO
