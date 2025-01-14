05bb8c48aab1206973c41c8404152015087ddc77
yangbranden <byang64@asu.edu>
2025-01-14 13:04:49 -0700

    statistics

--- MachineLearning/statistics.py
+++ MachineLearning/statistics.py
@@ -3,6 +3,9 @@ import pandas as pd
 # Load the data
 data = pd.read_csv("data_all_1_14_2025.csv", low_memory=False)
 
+# FILTER DATA FOR SPECIFIC URL
+data = data[data['url'] == 'hxxp://sameerschool.org/ionos/iindex.html']
+
 # Summary statistics
 # print(data.describe(include='all'))
 
@@ -13,13 +16,13 @@ data = pd.read_csv("data_all_1_14_2025.csv", low_memory=False)
 # print(data['os'].value_counts())
 
 # Susceptibility rate for OS
-# os_susceptibility = data.groupby('os')['blocked'].mean().sort_values(ascending=False)
-# print(os_susceptibility)
+os_susceptibility = data.groupby('os')['blocked'].mean().sort_values(ascending=False)
+print("OS STATISTICS", os_susceptibility)
 
 # Susceptibility rate for devices
-# device_susceptibility = data.groupby('device')['blocked'].mean().sort_values(ascending=False)
-# print(device_susceptibility)
+device_susceptibility = data.groupby('device')['blocked'].mean().sort_values(ascending=False)
+print("DEVICE STATISTICS", device_susceptibility)
 
 # Susceptibility rate for browser
 browser_susceptibility = data.groupby('browser')['blocked'].mean().sort_values(ascending=False)
-print(browser_susceptibility)
+print("BROWSER STATISTICS", browser_susceptibility)
