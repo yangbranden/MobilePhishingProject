e94f533853c218f41a218109c9dc4e1aeefa5a5d
yangbranden <byang64@asu.edu>
2025-01-14 12:58:09 -0700

    statistics

--- MachineLearning/statistics.py
+++ MachineLearning/statistics.py
@@ -1,13 +1,25 @@
 import pandas as pd
 
 # Load the data
-data = pd.read_csv("data_all_1_1_2025.csv", low_memory=False)
+data = pd.read_csv("data_all_1_14_2025.csv", low_memory=False)
 
 # Summary statistics
-print(data.describe(include='all'))
+# print(data.describe(include='all'))
 
 # Check for missing values
-print("ANYTHING HERE?", data.isnull().sum())
+# print("Checking null values:\n", data.isnull().sum())
 
 # Distribution of values
-print(data['os'].value_counts())
+# print(data['os'].value_counts())
+
+# Susceptibility rate for OS
+# os_susceptibility = data.groupby('os')['blocked'].mean().sort_values(ascending=False)
+# print(os_susceptibility)
+
+# Susceptibility rate for devices
+# device_susceptibility = data.groupby('device')['blocked'].mean().sort_values(ascending=False)
+# print(device_susceptibility)
+
+# Susceptibility rate for browser
+browser_susceptibility = data.groupby('browser')['blocked'].mean().sort_values(ascending=False)
+print(browser_susceptibility)
