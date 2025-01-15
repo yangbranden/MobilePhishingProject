13a1fd97cd6647e1351aa8e2c1a654ce8289cc30
yangbranden <byang64@asu.edu>
2025-01-14 18:45:34 -0700

    statistics

--- MachineLearning/statistics.py
+++ MachineLearning/statistics.py
@@ -3,8 +3,17 @@ import pandas as pd
 # Load the data
 data = pd.read_csv("data_all_1_14_2025.csv", low_memory=False)
 
-# FILTER DATA FOR SPECIFIC URL
-data = data[data['url'] == 'hxxp://sameerschool.org/ionos/iindex.html']
+# FILTER DATA FOR SPECIFIC URL (these are our phishing URLs)
+# data = data[data['url'] == 'hxxps://sedduok.github.io/test/']
+# data = data[data['url'] == 'hxxp://sameerschool.org/ionos/iindex.html']
+# data = data[data['url'] == 'hxxp://fysmganhfa.duckdns.org/ja/main']
+# data = data[data['url'] == 'hxxps://juquling.serv00.net/hypeSPAit24/hype/']
+# data = data[data['url'] == 'hxxps://bkq.cdj.mybluehost.me/']
+# data = data[data['url'] == 'hxxps://intlverifyngwebappsecurenewaccmanagespayments.jamulbryah.biz.id/signin']
+# data = data[data['url'] == 'hxxps://squid-app-luerc.ondigitalocean.app/login']
+# data = data[data['url'] == 'hxxps://swwsooledmansud.weebly.com/']
+# data = data[data['url'] == 'hxxps://jentif.com/strato/login']
+# data = data[data['url'] == 'hxxps://medexampass.com/routes/synwgxpl/envi-access/ss.html']
 
 # Summary statistics
 # print(data.describe(include='all'))
@@ -19,7 +28,7 @@ data = data[data['url'] == 'hxxp://sameerschool.org/ionos/iindex.html']
 os_susceptibility = data.groupby('os')['blocked'].mean().sort_values(ascending=False)
 print("OS STATISTICS", os_susceptibility)
 
-# Susceptibility rate for devices
+# Susceptibility rate for devices (TODO: we are grouping by individual device; I want to group by vendor (maybe we need to add another field for this))
 device_susceptibility = data.groupby('device')['blocked'].mean().sort_values(ascending=False)
 print("DEVICE STATISTICS", device_susceptibility)
 
